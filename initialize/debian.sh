d12df386e9f503694def32cf35fab7eb10a8cb4f
Martin Colwell <colwem@gmail.com>
2019-07-09 22:22:27 -0400

    yup

--- initialize/debian.sh
+++ initialize/debian.sh
@@ -4,6 +4,7 @@ sudo apt install stow neovim zsh zsh-antigen
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # curl -L git.io/antigen > antigen.zsh
 
 cd ~/_dotfiles
